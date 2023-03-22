6186e22dd5f4ebb8cdcd4b5c714491a3df3e2100
Oli Hock <oliver.hock@gmail.com>
2023-03-22 15:49:23 +0100

    test a scenario that submits a form

--- src/test/java/de/ithoc/training/selenium/WebFormTest.java
+++ src/test/java/de/ithoc/training/selenium/WebFormTest.java
@@ -1,20 +1,93 @@
 package de.ithoc.training.selenium;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class WebFormTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        // Create driver with the option to allow remote origins.
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+
+        // Navigate to this website which is a form.
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        // Wait a little to ensure website has been loaded.
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Clean up session
+        driver.quit();
+    }
+
 
     @Test
     public void startSessionChrome() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
 
-        Assertions.assertNotNull(driver);
+        assertNotNull(driver);
+    }
+
+    /**
+     * To run this test case successfully we need internet access,
+     * as we navigate to a website.
+     */
+    @Test
+    public void readTitle() {
+        // Read the title from this website.
+        String title = driver.getTitle();
+
+        assertEquals("Web form", title);
+    }
+
+    @Test
+    public void findTextBox() {
+        // Read a web element and check for text.
+        WebElement textBox = driver.findElement(By.name("my-text"));
+
+        assertEquals("", textBox.getText());
+    }
+
+    @Test
+    public void findButton() {
+        WebElement button = driver.findElement(By.cssSelector("button"));
+
+        assertEquals("Submit", button.getText());
+    }
+
+    /**
+     * Scenario: Find the text box, put some text in and send the form
+     * by pressing the button.
+     */
+    @Test
+    public void inputAndSubmit() {
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        textBox.sendKeys("Learning");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
     }
 
 }
