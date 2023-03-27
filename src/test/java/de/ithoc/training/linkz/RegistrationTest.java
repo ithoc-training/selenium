bf2315cc504314dac2b6a8df71f0f130c8f121f4
Oli Hock <oliver.hock@gmail.com>
2023-03-27 13:38:09 +0200

    implement test case password too short

--- src/test/java/de/ithoc/training/linkz/RegistrationTest.java
+++ src/test/java/de/ithoc/training/linkz/RegistrationTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RegistrationTest {
 
     @Test
@@ -47,10 +49,50 @@ public class RegistrationTest {
         // Check expected results (URL and message text).
 
         String currentUrl = driver.getCurrentUrl();
-        Assertions.assertEquals("http://localhost:8080/?registered=true", currentUrl);
+        assertEquals("http://localhost:8080/?registered=true", currentUrl);
 
         WebElement messageElement = driver.findElement(By.className("message-body"));
-        Assertions.assertEquals("You are now registered. Unfortnuately, there is no login yet...", messageElement.getText());
+        assertEquals("You are now registered. Unfortnuately, there is no login yet...", messageElement.getText());
+    }
+
+
+    @Test
+    public void registerPasswordTooShort() {
+
+        // Create web driver instance using remote origins.
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        // Navigate to website under test.
+        String url = "http://localhost:8080/register";
+        driver.get(url);
+
+        // Prepare test data (here username and password).
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+
+
+        // Run test scenario
+
+        WebElement usernameElement = driver.findElement(By.id("username"));
+        usernameElement.sendKeys(uuid);
+        String password = uuid.substring(uuid.length() - 4);
+
+        WebElement password1Element = driver.findElement(By.id("password1"));
+        password1Element.sendKeys(password);
+
+        WebElement password2Element = driver.findElement(By.id("password2"));
+        password2Element.sendKeys(password);
+
+        WebElement buttonElement = driver.findElement(By.tagName("button"));
+        buttonElement.click();
+
+
+        // Check expected results (URL and message text).
+
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals("http://localhost:8080/register", currentUrl);
     }
 
 }
