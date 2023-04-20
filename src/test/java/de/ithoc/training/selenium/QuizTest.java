2d35344df484a5c44abd1818034c1a5ddc4db6f8
Oli Hock <oliver.hock@gmail.com>
2023-04-20 09:43:56 +0200

    add selenium quiz as junit test class

---
+++ src/test/java/de/ithoc/training/selenium/QuizTest.java
@@ -0,0 +1,98 @@
+package de.ithoc.training.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class QuizTest {
+
+    private WebDriver webDriver;
+
+    @BeforeEach
+    public void setUp() {
+        // Create driver with the option to allow remote origins.
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+
+        // Navigate to this example website which is a form.
+        webDriver.get("http://localhost:8080/register");
+
+        // Wait a little to ensure website has been loaded.
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Clean up session
+        webDriver.quit();
+    }
+
+
+    @Test
+    public void findEmailById() {
+        // Read a web element and check for text.
+        WebElement textBox = webDriver.findElement(By.id("email-id"));
+
+        assertEquals("", textBox.getText());
+    }
+
+
+    /**
+     * To run this test case successfully we need internet access,
+     * as we navigate to a website.
+     */
+    @Test
+    public void readTitle() {
+        // Read the title from this website.
+        String title = webDriver.getTitle();
+
+        assertEquals("Web form", title);
+    }
+
+    @Test
+    public void findTextBox() {
+        // Read a web element and check for text.
+        WebElement textBox = webDriver.findElement(By.name("my-text"));
+
+        assertEquals("", textBox.getText());
+    }
+
+    @Test
+    public void findButton() {
+        WebElement button = webDriver.findElement(By.cssSelector("button"));
+
+        assertEquals("Submit", button.getText());
+    }
+
+    /**
+     * Scenario: Input field and submit button
+     * <p>
+     * Find the text box, put some text in and send the form
+     * by pressing the button.
+     */
+    @Test
+    public void inputAndSubmit() {
+        WebElement textBox = webDriver.findElement(By.name("my-text"));
+        textBox.sendKeys("Learning");
+
+        WebElement submitButton = webDriver.findElement(By.cssSelector("button"));
+        submitButton.click();
+
+        WebElement message = webDriver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+    }
+
+}
